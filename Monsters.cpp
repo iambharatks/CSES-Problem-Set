665aa24794514a2ce37d72b6e0f8d29f5bb36af4
iambharatks <iambharatks@gmail.com>
2022-03-14 09:25:30 +0530

    Monsters

--- Monsters.cpp
+++ Monsters.cpp
@@ -1,111 +1,164 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <climits>
-#include <cstring>
-#include <algorithm>
-#define pii pair<int, int>
-#define mn 1005
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 
-int N, M;
-queue<pii> q;
-int paths[mn][mn];
-pii from[mn][mn];
-int oo = INT_MAX;
-pii A;
-string ans;
-bool possible = false;
+#define iambharatks                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
 
-void retrace(pii node)
-{ // retrace from final node, adding direction from previous node to a string. This string will be backwards but will be reversed before output.
-    pii origin = from[node.first][node.second];
-    if (origin == pii(0, 0))
-        return;
-    if (origin.first == node.first + 1)
-        ans.push_back('U');
-    if (origin.first == node.first - 1)
-        ans.push_back('D');
-    if (origin.second == node.second + 1)
-        ans.push_back('L');
-    if (origin.second == node.second - 1)
-        ans.push_back('R');
-    retrace(origin);
-}
-void check(pii origin, pii dest)
-{ // check if the considered destination may be traveled to
-    int pl = paths[origin.first][origin.second];
-    if (pl + 1 < paths[dest.first][dest.second])
-    {
-        paths[dest.first][dest.second] = pl + 1;
-        q.push(dest);
-        from[dest.first][dest.second] = origin;
-    }
-}
-bool mora = false; // false if bfs for monsters, true if bfs for A
-void bfs()
+#define bitcount __builtin_popcount
+#define setbits(x) __builtin_popcountll(x)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define fo(i, j, n) for (ll i = j; i <= n; i++)
+#define rev(i, j, n) for (ll i = n; i >= j; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define INF 2e18
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define read(a)       \
+    for (auto &i : a) \
+        cin >> i;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<pll>> vvpll;
+typedef vector<pll> vpll;
+typedef vector<pi> vpi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+
+const int MOD = 998244353;
+const int N = 2e5 + 5;
+double eps = 1e-12;
+const int mod = 1e9 + 7;
+
+ll dr[4] = {-1, 1, 0, 0};
+ll dc[4] = {0, 0, -1, 1};
+char d[4] = {'D', 'U', 'R', 'L'};
+void solve()
 {
+    ll n, m, sr, sc;
+    cin >> n >> m;
+    vector<string> mat(n, "");
+    queue<pll> q;
+    int i = 0;
+    vvll dist(n, vll(m, INT_MAX)), M(n, vll(m, INT_MAX));
+    vector<vpll> parent(n, vpll(m, {-1, -1}));
+    for (string &s : mat)
+    {
+        cin >> s;
+        for (int j = 0; j < m; j++)
+        {
+            if (mat[i][j] == 'M')
+            {
+                q.push({i, j});
+                dist[i][j] = 0;
+            }
+            else if (mat[i][j] == 'A')
+            {
+                sr = i, sc = j;
+                M[sr][sc] = 0;
+            }
+        }
+        i++;
+    }
     while (!q.empty())
     {
-        pii loc = q.front(), next;
+        ll r = q.front().F, c = q.front().S;
         q.pop();
-        next = loc;
-        next.first++;
-        check(loc, next); // go through adjacent locations
-        next = loc;
-        next.first--;
-        check(loc, next);
-        next = loc;
-        next.second++;
-        check(loc, next);
-        next = loc;
-        next.second--;
-        check(loc, next);
-        if (mora && (loc.first == 1 || loc.second == 1 || loc.first == N || loc.second == M))
+        fo(i, 0, 3)
         {
-            cout << "YES" << endl;
-            cout << paths[loc.first][loc.second] << endl;
-            retrace(loc);
-            possible = true;
-            return;
+            ll nr, nc;
+            nr = r + dr[i];
+            nc = c + dc[i];
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m || mat[nr][nc] == '#')
+                continue;
+            if (dist[nr][nc] > dist[r][c] + 1)
+            {
+                dist[nr][nc] = dist[r][c] + 1;
+                q.push({nr, nc});
+            }
         }
     }
+    q.push({sr, sc});
+    ll er = -1, ec = -1;
+    while (!q.empty())
+    {
+        ll r = q.front().F, c = q.front().S;
+        q.pop();
+        if (r == n - 1 || c == m - 1 || r == 0 || c == 0)
+        {
+            er = r, ec = c;
+            break;
+        }
+        fo(i, 0, 3)
+        {
+            ll nr, nc;
+            nr = r + dr[i];
+            nc = c + dc[i];
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m || mat[nr][nc] == '#' || mat[nr][nc] == 'M')
+                continue;
+            if (dist[nr][nc] > M[r][c] + 1)
+            {
+                q.push({nr, nc});
+                M[nr][nc] = M[r][c] + 1;
+                parent[nr][nc] = {r, c};
+            }
+        }
+    }
+    if (er == -1)
+    {
+        cout << "NO\n";
+        return;
+    }
+    pll par = {er, ec};
+    string ans = "";
+    while (par != mp(sr, sc) && par.F != -1)
+    {
+        fo(i, 0, 3)
+        {
+            if (parent[par.F][par.S] == mp(par.F + dr[i], par.S + dc[i]))
+            {
+                ans.push_back(d[i]);
+                break;
+            }
+        }
+        par = parent[par.F][par.S];
+    }
+    cout << "YES\n";
+    cout << sz(ans) << ln;
+    reverse(all(ans));
+    cout << ans << ln;
 }
+
 int main()
 {
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++)
+    iambharatks;
+    ll t = 1;
+    // cin >> t;
+    for (int it = 1; it <= t; it++)
     {
-        string s;
-        cin >> s;
-        for (int j = 1; j <= M; j++)
-        {
-            paths[i][j] = oo;
-            if (s[j - 1] == '#')
-                paths[i][j] = 0;
-            if (s[j - 1] == 'M')
-            {
-                q.push(pii(i, j));
-                paths[i][j] = 0;
-            }
-            if (s[j - 1] == 'A')
-            {
-                A.first = i;
-                A.second = j;
-            }
-        }
+        // cout << "Case #" << it << ": ";
+        solve();
     }
-    bfs();                               // monster bfs
-    mora = true;                         // change next bfs to A bfs
-    from[A.first][A.second] = pii(0, 0); // give the retrace a terminating location
-    paths[A.first][A.second] = 0;
-    q.push(A); // get ready for next bfs
-    bfs();     // bfs with A
-    if (possible)
-    {
-        reverse(ans.begin(), ans.end());
-        cout << ans << endl;
-    }
-    else
-        cout << "NO" << endl;
+    return 0;
 }
