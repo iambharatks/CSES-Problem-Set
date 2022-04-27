600b427f2a68d99529bd9342075a1a5137340a08
iambharatks <iambharatks@gmail.com>
2022-04-27 18:59:56 +0530

    Array Description

---
+++ Array_Description.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define iambharatks                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define bitcount __builtin_popcount
+#define setbits(x) __builtin_popcountll(x)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define fo(i, j, n) for (ll i = j; i <= n; i++)
+#define rev(i, j, n) for (ll i = n; i >= j; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define INF 2e18
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define read(a)       \
+    for (auto &i : a) \
+        cin >> i;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<pll>> vvpll;
+typedef vector<pll> vpll;
+typedef vector<pi> vpi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+
+const int MOD = 998244353;
+const int N = 2e5 + 5;
+double eps = 1e-12;
+const int mod = 1e9 + 7;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    vll x(n);
+    fo(i, 0, n - 1) cin >> x[i];
+    vvi dp(n + 1, vi(m + 1));
+    fo(i, 1, m)
+        dp[1][i] = (x[0] != 0 && x[0] != i) ? 0 : 1;
+
+    fo(i, 2, n)
+    {
+        fo(j, 1, m)
+        {
+            if (x[i - 1] != 0 && x[i - 1] != j)
+                dp[i][j] = 0;
+            else
+                dp[i][j] = ((dp[i - 1][j - 1] + dp[i - 1][j]) % mod + dp[i - 1][j + 1]) % mod;
+        }
+    }
+    ll res = 0;
+    fo(i, 0, m) res = (res + dp[n][i]) % mod;
+    cout << res << ln;
+}
+
+int main()
+{
+    iambharatks;
+    ll t = 1;
+    // cin >> t;
+    for (int it = 1; it <= t; it++)
+    {
+        // cout << "Case #" << it << ": ";
+        solve();
+    }
+    return 0;
+}
